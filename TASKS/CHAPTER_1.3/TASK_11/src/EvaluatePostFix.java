77fb7481d21f3ae23d853076f771276ad166fc7a
Konstantin Valiulin <Clash19856@gmail.com>
2024-04-05 16:41:26 +0300

    TASK11 - COMPLETE

---
+++ TASKS/CHAPTER_1.3/TASK_11/src/EvaluatePostFix.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class EvaluatePostFix {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        System.out.println(num);
+    }
+}
