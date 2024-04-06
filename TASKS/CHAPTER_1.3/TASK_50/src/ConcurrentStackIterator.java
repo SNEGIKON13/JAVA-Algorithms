701eb085591bac568bc320900f1ba64c1f29849d
Konstantin Valiulin <Clash19856@gmail.com>
2024-04-06 09:46:01 +0300

    TASK50 - COMPLETE

---
+++ TASKS/CHAPTER_1.3/TASK_50/src/ConcurrentStackIterator.java
@@ -0,0 +1,30 @@
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.Stack;
+
+public class ConcurrentStackIterator<T> implements Iterator<T> {
+    private Stack<T> stack;
+    private int counter;
+    public ConcurrentStackIterator(Stack <T> stack) {
+    this.stack = stack;
+    this.counter = stack.size();
+    }
+
+    @Override
+    public boolean hasNext() {
+
+        return stack.iterator().hasNext();
+    }
+
+    @Override
+    public T next() {
+        checkModification();
+        return stack.iterator().next();
+    }
+
+    private void checkModification() {
+        if (counter != stack.size()) {
+            throw new ConcurrentModificationException();
+        }
+    }
+}
