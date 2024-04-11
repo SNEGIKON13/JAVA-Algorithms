53382f9635e72b36a27b35b47e3df47aade2db93
Konstantin Valiulin <Clash19856@gmail.com>
2024-04-11 09:44:13 +0300

    TASK27 - COMPLETE

---
+++ chapter_2.5/task_27/src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите строку чисел разделяемых пробелом:");
+        String str = input.nextLine();
+        String[] strs = str.split(" ");
+        Integer[] array = new Integer[strs.length];
+        for (int i = 0; i < strs.length; i++) {
+            array[i] = Integer.parseInt(strs[i]);
+        }
+        int[] index = ParallelArraySort.indirectSort(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(index[i] + " ");
+        }
+    }
+}
---
+++ chapter_2.5/task_27/src/main/java/org/example/ParallelArraySort.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class ParallelArraySort {
+
+    public static <T extends Comparable<T>> int[] indirectSort(T[] array) {
+        int[] index = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            index[i] = i;
+        }
+        int arraySize = array.length;
+        for (int i = 1; i < arraySize; i++) {
+            int keyIndex = index[i];
+            T key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j].compareTo(key) > 0) {
+                array[j + 1] = array[j];
+                index[j + 1] = index[j];
+                j = j - 1;
+            }
+
+            array[j + 1] = key;
+            index[j + 1] = keyIndex;
+        }
+        return index;
+    }
+}
